5cca1809d5c9a70de3dc1dbba968c6e8a3bd7ec1
Sayam Sethi <sayam.sethi2@gmail.com>
2025-09-11 22:12:01 -0500

    updated nvdash

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -24,25 +24,7 @@ M.nvdash = {
     "                      ",
   },
 
-  buttons = {
-    { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
-    { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
-    { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
-    { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
-
-    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
-    {
-      txt = function()
-        local stats = require("lazy").stats()
-        local ms = math.floor(stats.startuptime) .. " ms"
-        return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
-      end,
-      hl = "NvDashFooter",
-      no_gap = true,
-      content = "fit",
-    },
-    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
-  },
+  buttons = require "nvdash",
 }
 
 M.cheatsheet = {
