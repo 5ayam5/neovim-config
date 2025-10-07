61139637a8f659fbe2f4223bc2b17a4d032009ee
Sayam Sethi <sayam.sethi2@gmail.com>
2025-10-07 13:24:44 -0500

    updated markview config

--- lua/configs/markview.lua
+++ lua/configs/markview.lua
@@ -4,9 +4,22 @@ dofile(vim.g.base46_cache .. "markview")
 ---@type markview.config
 local M = {}
 
-M.markdown = { ---@diagnostic disable-line: missing-fields
-  headings = require("markview.presets").headings.numbered,
-}
+M.markdown = require "markview.config.markdown"
+
+M.markdown.headings = require("markview.presets").headings.numbered
+M.markdown.list_items.marker_minus.text = function(_, item) ---@diagnostic disable-line: assign-type-mismatch
+  ---@cast item markview.parsed.markdown.list_items
+  local index = (item.indent / 2) % 4
+  if index == 0 then
+    return "●"
+  elseif index == 1 then
+    return "○"
+  elseif index == 2 then
+    return "◈"
+  elseif index == 3 then
+    return "◇"
+  end
+end
 
 M.preview = {
   icon_provider = "devicons",
