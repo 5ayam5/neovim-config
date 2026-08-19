3ea68968f290b626ac6504691830d463e0fd46cd
Sayam Sethi <sayam.sethi2@gmail.com>
2026-08-19 13:36:09 -0500

    fix: better display inside venv_selector

--- lua/configs/venv_selector.lua
+++ lua/configs/venv_selector.lua
@@ -1,16 +1,23 @@
 ---@module "venv-selector"
+
+local function conda_env_name(path)
+  return path:match "/envs/([^/]+)/" or "base"
+end
+
 return {
   options = {
     picker = "snacks",
   },
   search = {
-    miniconda_envs = {
+    conda = {
       command = "$FD 'bin/python$' /opt/miniconda3/envs --no-ignore-vcs --full-path --color never",
       type = "anaconda",
+      on_telescope_result_callback = conda_env_name,
     },
-    miniconda_base = {
+    base = {
       command = "$FD '/python$' /opt/miniconda3/bin --no-ignore-vcs --full-path --color never",
       type = "anaconda",
+      on_telescope_result_callback = conda_env_name,
     },
   },
 }
